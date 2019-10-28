3ad182b6b83c1f5813fcc568b15a977b20f12cb1
mt02683 <maggie02683@gmail.com>
2019-10-28 20:34:57 +0800

    Refactor code according to MVC design patterns

---
+++ Controller/LevelController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BabaIsYou.Model;
+
+namespace BabaIsYou.Controller
+{
+    class LevelController
+    {
+
+        Model.Level level;
+
+        public LevelController(int levelNumber)
+        {
+            level = new Model.Level(levelNumber);
+        }
+        public void loadGame()
+        {
+            //load blocks from data into CurrentMap
+            //... 
+            //MapHeight=
+            //MapWidth=
+            level.MapHeight = 18;
+            level.MapWidth = 18;
+            level.CurrentMap.PointBlockPairs.Add((5, 6), new List<Block> { new Block.Text.TextBaba() });
+            level.CurrentMap.PointBlockPairs.Add((6, 6), new List<Block> { new Block.Text.TextIs() });
+            level.CurrentMap.PointBlockPairs.Add((7, 6), new List<Block> { new Block.Text.TextYou() });
+            level.CurrentMap.PointBlockPairs.Add((12, 6), new List<Block> { new Block.Text.TextFlag() });
+            level.CurrentMap.PointBlockPairs.Add((13, 6), new List<Block> { new Block.Text.TextIs() });
+            level.CurrentMap.PointBlockPairs.Add((14, 6), new List<Block> { new Block.Text.TextWin() });
+
+            level.CurrentMap.PointBlockPairs.Add((9, 9), new List<Block> { new Block.Thing.Rock() });
+
+
+            level.CurrentMap.PointBlockPairs.Add((6, 10), new List<Block> { new Block.Thing.Baba() });
+
+            AddToHistory();
+        }
+        public bool CheckWin()
+        {
+            //...   
+            return false;
+        }
+        public void MoveBlocks(string direction) { }
+        public void UpdateRules()
+        {
+            //first reset the rules and then find the new rules and apply them
+        }
+        public void UpdateBlocks() { }
+        public void AddToHistory()
+        {
+            level.History.Add(level.CurrentMap);
+        }
+        public void GoBack()
+        {
+            if (level.History.Count > 1)
+            {
+                level.History.RemoveAt(level.History.Count - 1);
+                level.CurrentMap = level.History[level.History.Count - 1];
+            }
+        }
+        public void Draw()
+        {
+            // MessageBox.Show("Test");
+        }
+    }
+}
---
+++ Model/Block.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabaIsYou.Model
+{
+    class Block
+    {
+        public bool IsPush { get; protected set; }
+        public bool IsStop { get; protected set; }
+        public bool IsYou { get; protected set; }
+        public bool IsWin { get; protected set; }
+        public string imgsrc { get; protected set; }
+        public Block()
+        {
+            IsWin = false;
+            IsYou = false;
+        }
+
+        public class Text : Block
+        {
+            public Text()
+            {
+                IsPush = true;
+                IsStop = true;
+            }
+
+            public class TextBaba : Text
+            {
+                public TextBaba()
+                {
+                    imgsrc = "";
+                }
+            }
+            public class TextRock : Text
+            {
+                public TextRock()
+                {
+                    imgsrc = "";
+                }
+            }
+            public class TextFlag : Text
+            {
+                public TextFlag()
+                {
+                    imgsrc = "";
+                }
+            }
+            public class TextWall : Text
+            {
+                public TextWall()
+                {
+                    imgsrc = "";
+                }
+            }
+            public class TextIs : Text
+            {
+                public TextIs()
+                {
+                    imgsrc = "";
+                }
+            }
+            public class TextYou : Text
+            {
+                public TextYou()
+                {
+                    imgsrc = "";
+                }
+            }
+            public class TextWin : Text
+            {
+                public TextWin()
+                {
+                    imgsrc = "";
+                }
+            }
+            public class TextPush : Text
+            {
+                public TextPush()
+                {
+                    imgsrc = "";
+                }
+            }
+            public class TextStop : Text
+            {
+                public TextStop()
+                {
+                    imgsrc = "";
+                }
+            }
+        }
+
+        public class Thing : Block
+        {
+            public Thing()
+            {
+                IsPush = false;
+                IsStop = false;
+            }
+
+            public class Baba : Thing
+            {
+                public Baba()
+                {
+                    imgsrc = "";
+                }
+            }
+            public class Rock : Thing
+            {
+                public Rock()
+                {
+                    imgsrc = "";
+                }
+            }
+            public class Flag : Thing
+            {
+                public Flag()
+                {
+                    imgsrc = "";
+                }
+            }
+            public class Wall : Thing
+            {
+                public Wall()
+                {
+                    imgsrc = "";
+                }
+            }
+        }
+    }
+}
