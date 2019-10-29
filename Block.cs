ceb0d6dadca31c24d25add63d1b0ea662ee0e3e0
Crist Ye <iamCristYe@Outlook.com>
2019-10-30 00:43:30 +0800

    implement some functions

---
+++ Block.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabaIsYou.Model
+{
+    class Block
+    {
+        public bool IsPush { get; protected set; }
+        public bool IsStop { get; protected set; }
+        public bool IsYou { get; protected set; }
+        public bool IsWin { get; protected set; }
+        public bool IsSink { get; protected set; }
+        public bool IsKill { get; protected set; }
+        public bool IsMove { get; protected set; }
+        //slip hot melt
+        public string imgsrc { get; protected set; }
+        public Block()
+        {
+            IsWin = false;
+            IsYou = false;
+            IsSink = false;
+            IsKill = false;
+            IsMove = false; 
+        }
+
+        public class Text : Block
+        {
+            public Text()
+            {
+                IsPush = true;
+                IsStop = true;
+            }
+
+            public class TextBaba : Text
+            {
+                public TextBaba()
+                {
+                    imgsrc = "/Resources/icons/BABA_TEXT.jpg";
+                }
+            }
+            public class TextRock : Text
+            {
+                public TextRock()
+                {
+                    imgsrc = "/Resources/icons/ROCK_TEXT.jpg";
+                }
+            }
+            public class TextFlag : Text
+            {
+                public TextFlag()
+                {
+                    imgsrc = "/Resources/icons/FLAG_TEXT.jpg";
+                }
+            }
+            public class TextWall : Text
+            {
+                public TextWall()
+                {
+                    imgsrc = "/Resources/icons/WALL_TEXT.jpg";
+                }
+            }
+            public class TextIs : Text
+            {
+                public TextIs()
+                {
+                    imgsrc = "/Resources/icons/IS_TEXT.jpg";
+                }
+            }
+            public class TextYou : Text
+            {
+                public TextYou()
+                {
+                    imgsrc = "/Resources/icons/YOU_TEXT.jpg";
+                }
+            }
+            public class TextWin : Text
+            {
+                public TextWin()
+                {
+                    imgsrc = "/Resources/icons/WIN_TEXT.jpg";
+                }
+            }
+            public class TextPush : Text
+            {
+                public TextPush()
+                {
+                    imgsrc = "/Resources/icons/PUSH_TEXT.jpg";
+                }
+            }
+            public class TextStop : Text
+            {
+                public TextStop()
+                {
+                    imgsrc = "/Resources/icons/STOP_TEXT.jpg";
+                }
+            }
+        }
+
+        public class Thing : Block
+        {
+            public Thing()
+            {
+                IsPush = false;
+                IsStop = false;
+            }
+
+            public class Baba : Thing
+            {
+                public Baba()
+                {
+                    imgsrc = "/Resources/icons/BABA_THING.jpg";
+                }
+            }
+            public class Rock : Thing
+            {
+                public Rock()
+                {
+                    imgsrc = "/Resources/icons/ROCK_THING.jpg";
+                }
+            }
+            public class Flag : Thing
+            {
+                public Flag()
+                {
+                    imgsrc = "/Resources/icons/FLAG_THING.jpg";
+                }
+            }
+            public class Wall : Thing
+            {
+                public Wall()
+                {
+                    imgsrc = "/Resources/icons/WALL_THING.jpg";
+                }
+            }
+        }
+    }
+}
---
+++ LevelController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BabaIsYou.Model;
+
+namespace BabaIsYou.Controller
+{
+    class LevelController
+    {
+
+        Model.Level level;
+
+        public LevelController(int levelNumber)
+        {
+            level = new Model.Level(levelNumber);
+        }
+        public void loadGame()
+        {
+            //load blocks from data into CurrentMap
+            //... 
+            //MapHeight=
+            //MapWidth=
+            level.MapHeight = 18;
+            level.MapWidth = 18;
+            level.CurrentMap.PointBlockPairs.Add((5, 6), new List<Block> { new Block.Text.TextBaba() });
+            level.CurrentMap.PointBlockPairs.Add((6, 6), new List<Block> { new Block.Text.TextIs() });
+            level.CurrentMap.PointBlockPairs.Add((7, 6), new List<Block> { new Block.Text.TextYou() });
+            level.CurrentMap.PointBlockPairs.Add((12, 6), new List<Block> { new Block.Text.TextFlag() });
+            level.CurrentMap.PointBlockPairs.Add((13, 6), new List<Block> { new Block.Text.TextIs() });
+            level.CurrentMap.PointBlockPairs.Add((14, 6), new List<Block> { new Block.Text.TextWin() });
+
+            level.CurrentMap.PointBlockPairs.Add((9, 9), new List<Block> { new Block.Thing.Rock() });
+
+
+            level.CurrentMap.PointBlockPairs.Add((6, 10), new List<Block> { new Block.Thing.Baba() });
+
+            AddToHistory();
+        }
+        public void MoveBlocks(string direction)
+        {
+
+
+        }
+        public void UpdateRules()//first reset the rules and then find the new rules and apply them
+        {
+
+        }
+        public void UpdateBlocks()//like sink/kill
+        {
+            //sink
+            for (int Row = 1; Row < level.MapWidth; Row++)
+            {
+                for (int Column = 1; Column < level.MapHeight; Column++)
+                {
+                    bool ContainsSinkBlock = false;
+                    foreach (var SinkBlock in level.CurrentMap.PointBlockPairs[(Row, Column)])
+                    {
+                        if (SinkBlock.IsSink == true) ContainsSinkBlock = true;
+                    }
+                    if (ContainsSinkBlock)//remove all blocks
+                    {
+                        level.CurrentMap.PointBlockPairs[(Row, Column)] = new List<Block>();
+                    }
+                }
+            }
+            //killblock removes youblock
+            for (int Row = 1; Row < level.MapWidth; Row++)
+            {
+                for (int Column = 1; Column < level.MapHeight; Column++)
+                {
+                    bool ContainsKillBlock = false;
+                    bool ContainsYouBlock = false;
+                    foreach (var KillBlock in level.CurrentMap.PointBlockPairs[(Row, Column)])
+                    {
+                        if (KillBlock.IsKill == true) ContainsKillBlock = true;
+                    }
+                    foreach (var YouBlock in level.CurrentMap.PointBlockPairs[(Row, Column)])
+                    {
+                        if (YouBlock.IsYou == true) ContainsYouBlock = true;
+                    }
+                    if (ContainsKillBlock && ContainsYouBlock)//remove you block
+                    {
+                        List<Block> NewList = new List<Block>();
+                        foreach (var NotYouBlock in level.CurrentMap.PointBlockPairs[(Row, Column)])
+                        {
+                            if (NotYouBlock.IsYou == false) NewList.Add(NotYouBlock);
+                        }
+                        level.CurrentMap.PointBlockPairs[(Row, Column)] = NewList;
+                    }
+                }
+            }
+        }
+        public bool CheckWin()
+        {
+            for (int Row = 1; Row < level.MapWidth; Row++)
+            {
+                for (int Column = 1; Column < level.MapHeight; Column++)
+                {
+                    bool ContainsWinBlock = false;
+                    bool ContainsYouBlock = false;
+                    foreach (var WinBlock in level.CurrentMap.PointBlockPairs[(Row, Column)])
+                    {
+                        if (WinBlock.IsWin == true) ContainsWinBlock = true;
+                    }
+                    foreach (var YouBlock in level.CurrentMap.PointBlockPairs[(Row, Column)])
+                    {
+                        if (YouBlock.IsYou == true) ContainsYouBlock = true;
+                    }
+                    if (ContainsWinBlock && ContainsYouBlock) return true;
+                }
+            }
+            return false;
+        }
+
+        public void AddToHistory()
+        {
+            level.History.Add(level.CurrentMap);
+        }
+        public void GoBack()
+        {
+            if (level.History.Count > 1)
+            {
+                level.History.RemoveAt(level.History.Count - 1);
+                level.CurrentMap = level.History[level.History.Count - 1];
+            }
+        }
+        public void Draw()
+        {
+            // MessageBox.Show("Test");
+        }
+    }
+}
