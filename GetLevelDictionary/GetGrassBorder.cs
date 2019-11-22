27a83710f1f6bc9a8fdfd3a478073a3737e7ca0b
mt02683 <maggie02683@gmail.com>
2019-11-22 10:49:17 +0800

    Fixed GetGrassBorder method parameter bug

--- GetLevelDictionary/GetGrassBorder.cs
+++ GetLevelDictionary/GetGrassBorder.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 class GetGrassBorder
 {
-    public static void AddGrass(Dictionary<(int, int), List<Block>> _dict)
+    public static void AddGrass(Dictionary<(int, int), List<Block>> dict)
     {
       // vertical columns of grass (left most and right most column)
       for (int i = 0; i < 20; i += 1)
